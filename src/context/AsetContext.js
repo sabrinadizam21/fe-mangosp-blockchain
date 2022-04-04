4c6baf88ec3be2b77e321ac314e94063808126c9
sabrinadizam21 <sabrinadizam21@gmail.com>
2022-04-04 12:16:09 +0700

    add kuantitasBenih param to aset context

--- src/context/AsetContext.js
+++ src/context/AsetContext.js
@@ -2,10 +2,10 @@ import { useState, createContext } from 'react'
 export const AsetContext = createContext()
 export const AsetProvider = props => {
     const [aset,setAset] = useState([
-        {varietas :'Benih A', umurBenih : 20, umurPanen : 1, hargaPanen : 101500},
-        {varietas :'Jaya Raya', umurBenih : 10, umurPanen : 2, hargaPanen : 130000},
-        {varietas :'Benih C', umurBenih : 14, umurPanen : 3, hargaPanen : 12000},
-        {varietas :'Benih K', umurBenih : 18, umurPanen : 4, hargaPanen : 104000}
+        {varietas :'Benih A', kuantitasBenih : 1000, umurBenih : 20, umurPanen : 1, hargaPanen : 101500},
+        {varietas :'Jaya Raya', kuantitasBenih : 1200, umurBenih : 10, umurPanen : 2, hargaPanen : 130000},
+        {varietas :'Benih C', kuantitasBenih : 300, umurBenih : 14, umurPanen : 3, hargaPanen : 12000},
+        {varietas :'Benih K', kuantitasBenih : 540, umurBenih : 18, umurPanen : 4, hargaPanen : 104000}
     ])
 
     //const [ loginStatus, setLoginStatus] = useState(false)
