19426c52ab2ec541f9ddb088aaff23dfe03f5780
sabrinadizam21 <sabrinadizam21@gmail.com>
2022-04-05 17:59:18 +0700

    looping data aset in aset page

--- src/context/AsetContext.js
+++ src/context/AsetContext.js
@@ -2,10 +2,10 @@ import { useState, createContext } from 'react'
 export const AsetContext = createContext()
 export const AsetProvider = props => {
     const [aset,setAset] = useState([
-        {varietas :'Benih A', kuantitasBenih : 1000, umurBenih : 20, umurPanen : 1, hargaPanen : 101500},
-        {varietas :'Jaya Raya', kuantitasBenih : 1200, umurBenih : 10, umurPanen : 2, hargaPanen : 130000},
-        {varietas :'Benih C', kuantitasBenih : 300, umurBenih : 14, umurPanen : 3, hargaPanen : 12000},
-        {varietas :'Benih K', kuantitasBenih : 540, umurBenih : 18, umurPanen : 4, hargaPanen : 104000}
+        {id : 0, createDate: '23 Maret 2022 - 23:59 WIB', varietas :'Benih A', kuantitasBenih : 1000, umurBenih : 20, umurPanen : 1, hargaPanen : 101500},
+        {id : 1, createDate: '1 April 2022 - 03:15 WIB', varietas :'Jaya Raya', kuantitasBenih : 1200, umurBenih : 10, umurPanen : 2, hargaPanen : 130000},
+        {id : 2, createDate: '12 Januari 2022 - 13:00 WIB', varietas :'Benih C', kuantitasBenih : 300, umurBenih : 14, umurPanen : 3, hargaPanen : 12000},
+        {id : 3, createDate: '24 Februari 2022 - 09:00 WIB', varietas :'Benih K', kuantitasBenih : 540, umurBenih : 18, umurPanen : 4, hargaPanen : 104000}
     ])
 
     //const [ loginStatus, setLoginStatus] = useState(false)
--- src/pages/Aset/Aset.js
+++ src/pages/Aset/Aset.js
@@ -1,13 +1,38 @@
-import React , { useState } from 'react'
+import React , { useState, useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { Button } from '../../components/Button'
 import { FaSeedling } from 'react-icons/fa'
 import './Aset.css'
 import Modal from '../../components/Modal'
 import { Input } from '../../components/Input'
+import { AsetContext } from '../../context/AsetContext'
 
 function Aset() {
-  const [modalOpen, setModalOpen] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false)
+  const { aset } = useContext(AsetContext)
+  const [inputData, setInputData] = useState("")
+  const [currentIndex, setCurrentIndex] = useState(-1)
+
+  const handleChange = (event) => {
+    let input = event.target.value
+    setInputData(input)
+}
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    let newData = aset
+        if(currentIndex === -1){
+            newData = [...aset, inputData]
+        }
+        else {
+            newData[currentIndex] = inputData
+        }
+  }
+
+  function numberFormat(number){
+    return number.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ".")
+  }
+
   return (
     <>
         <div className="aset__wrapper">
@@ -25,60 +50,67 @@ function Aset() {
                 </div>
               </div>
               <div className="aset__content">
-                <div className="card">
-                  <div className="card__header">
-                    <div className="card__icon">
-                      <FaSeedling className='card__logo' />
-                    </div>
-                    <div style={{marginLeft: '20px'}}>
-                      <b>Benih A</b>
-                      <p className="card__timestamp">23 Maret 2022 - 23:59 WIB</p>
-                    </div>                
-                  </div>
-                  <div className="card__body">
-                    <div className="quantity-value">
-                      <div className="quantity">
-                        <span>Kuantitas</span>
-                        <p>1000 Kg</p>
+                {aset.map((data, index)=>{
+                  return (
+                    <div className="card" key={data.id}>
+                      <div className="card__header">
+                        <div className="card__icon">
+                          <FaSeedling className='card__logo' />
+                        </div>
+                        <div style={{marginLeft: '20px'}}>
+                          <b>{data.varietas}</b>
+                          <p className="card__timestamp">{data.createDate}</p>
+                        </div>                
                       </div>
-                      <div className="value">
-                        <span>Harga per Kg</span>
-                        <p>Rp100,000</p>
+                      <div className="card__body">
+                        <div className="quantity-value">
+                          <div className="quantity">
+                            <span>Kuantitas</span>
+                            <p>{numberFormat(data.kuantitasBenih)} Kg</p>
+                          </div>
+                          <div className="value">
+                            <span>Harga per Kg</span>
+                            <p>Rp{numberFormat(data.hargaPanen)}</p>
+                          </div>
+                        </div>
+                        <div className="seed-age">
+                          <span>Umur Benih</span> 
+                          <p>{numberFormat(data.umurBenih)} hari</p>
+                        </div>
+                        <div className="harvest-age">
+                          <span>Umur Panen</span>
+                          <p>{numberFormat(data.umurPanen)} hari</p>
+                        </div>
+                      </div>
+                      <div className="card__bottom">
+                          <Button className="openModalBtn" buttonSize={'btn--primary'}
+                            onClick={() => {
+                              setModalOpen(true);
+                            }}
+                          > 
+                          TAMBAH KUANTITAS
+                          </Button>
+                        {modalOpen && 
+                          <Modal setOpenModal={setModalOpen} 
+                            modalTitle={'Tambah Kuantitas'}  
+                            modalBody={
+                              <>
+                                <p>Kuantitas Benih A saat ini : {data.kuantitasBenih} Kg</p>
+                                <br />
+                                <form onSubmit={handleSubmit}>
+                                  <Input className='number' label={'Kuantitas Benih Baru'} type='number' name='kuantitasBenih' id='kuantitasBenih' 
+                                  placeholder='Kuantitas Benih Baru' value={inputData} onChange={handleChange} required />
+                                </form>
+                              </>
+                            } 
+                            cancelBtn ={'BATAL'}
+                            processBtn={'SIMPAN'}
+                            typePrcsBtn={'SUBMIT'}
+                          />}
                       </div>
                     </div>
-                    <div className="seed-age">
-                      <span>Umur Benih</span> 
-                      <p>20 hari</p>
-                    </div>
-                    <div className="harvest-age">
-                      <span>Umur Panen</span> <p>20 hari</p>
-                    </div>
-                  </div>
-                  <div className="card__bottom">
-                      <Button className="openModalBtn"
-                        onClick={() => {
-                          setModalOpen(true);
-                        }}
-                      > 
-                      TAMBAH KUANTITAS
-                      </Button>
-                    {modalOpen && 
-                      <Modal setOpenModal={setModalOpen} 
-                        modalTitle={'Tambah Kuantitas'}  
-                        modalBody={
-                          <>
-                            <p>Kuantitas Benih A saat ini : 1.000 Kg</p>
-                            <br />
-                            <Input className='number' label={'Kuantitas Benih Baru'} type='number' name='kuantitasBenih' id='kuantitasBenih' 
-                              placeholder='Kuantitas Benih Baru' required />
-                          </>
-                        } 
-                        cancelBtn ={'BATAL'}
-                        processBtn={'SIMPAN'}
-                        typePrcsBtn={'SUBMIT'}
-                      />}
-                  </div>
-                </div> 
+                  )
+                })}
               </div>
             </div>
         </div>
