d61e6aac0e6a6a7f54b4827201fb8d8fed6b2ec4
sabrinadizam21 <sabrinadizam21@gmail.com>
2022-06-06 22:10:46 +0700

    delete data.js

--- src/pages/Home/HeroSection.js
+++ src/pages/Home/HeroSection.js
@@ -1,29 +1,23 @@
-import React, { useContext,useEffect } from 'react';
+import React, { useContext,useEffect,useState } from 'react';
 import './HeroSection.css';
 import { Button } from '../../components/Button';
 import { Link } from 'react-router-dom';
 import { UserContext } from '../../context/UserContext'
 import Cookies from 'js-cookie';
 
-function HeroSection({
-  lightBg,
-  lightText,
-  lightTextDesc,
-  headline,
-  buttonLabel,
-  img,
-  alt,
-  imgStart
-}) {
-  const { profile, functionUser } = useContext(UserContext)
+function HeroSection() {
+  const { profile, functionUser, loginStatus } = useContext(UserContext)
   const { getUserLogin } = functionUser
-
+  const [ lightBg ] = useState(false)
+  const [ lightTextDesc ] = useState(true)
+  const [ lightText ] = useState(true)
+  const [ imgStart ] = useState('')
+  
   const username = Cookies.get('username')
-  useEffect(async()=>{
+  useEffect(()=>{
     getUserLogin(username)
   }, [])
-  
-  console.log(profile)
+
   return (
     <>
       <div
@@ -40,7 +34,7 @@ function HeroSection({
             <div className='col'>
               <div className='home__hero-text-wrapper'>
                 <h1 className={lightText ? 'heading' : 'heading dark'}>
-                  {headline}
+                  Rantai Pasok Mangga
                 </h1>
                 <p
                   className={
@@ -49,12 +43,12 @@ function HeroSection({
                       : 'home__hero-subtitle dark'
                   }
                 >
-                  { profile !== '' ? (`Selamat Datang, ${profile.namaLengkap}!`) : (`Selamat Datang!`)}
+                  { !loginStatus || username === undefined ? (`Selamat Datang!`) : (`Selamat Datang, ${profile.namaLengkap}!`)}
                   
                 </p>
                 <Link to='/'>
                   <Button buttonSize='btn--wide' buttonColor='primary'>
-                    {buttonLabel}
+                    Scan QR Code
                   </Button>
                 </Link>
               </div>
@@ -62,7 +56,7 @@ function HeroSection({
             <div className='col'>
               <div className='home__hero-img-wrapper'>
                 <div className="home__hero-tooltip">
-                  <img src={img} alt={alt} className='home__hero-img'/>
+                  <img src={'mango.png'} alt={'Mangga'} className='home__hero-img'/>
                   <span className='home__hero-tooltiptext'>Background vector created by freepik - www.freepik.com</span>
                 </div>
               </div>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import HeroSection from './HeroSection'
-import {jumbotron} from './Data'
 import Feature from './Feature'
 import ScanQRCode from './ScanQRCode'
 
 function Home() {
   return (
     <>
-        <HeroSection {...jumbotron}/>
+        <HeroSection/>
         <ScanQRCode />
         <Feature />
     </>
