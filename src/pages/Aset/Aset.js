01d1c47d375836fe724d73bc35f8a794fda76a7c
sabrinadizam21 <sabrinadizam21@gmail.com>
2022-09-13 15:36:14 +0700

    delete daftar benih & update qty

--- src/pages/Aset/Aset.js
+++ src/pages/Aset/Aset.js
@@ -37,7 +37,7 @@ function Aset() {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    addQtyBenih(getId)
+    // addQtyBenih(getId)
     setModalOpen(false)
     setConfirmed(true)
     validateInput(e)
@@ -78,80 +78,12 @@ function Aset() {
                   <div className="title">Aset</div>
                   <div className="subtitle">Aset yang Anda miliki</div>
                 </div>
-                <UnlockAccess request={'Org1'}>
-                  <div className="btn-header">
-                    <Link to='/aset/daftaraset'>
-                      <Button buttonColor='primary'>DAFTAR ASET</Button>
-                    </Link>
-                  </div>
-                </UnlockAccess>
               </div>
               <div className="content">
                 <div className="card__wrapper">
                   {/* START ASET PENANGKAR */}
                   <UnlockAccess request={'Org1'}>
-                    {dataAset.length === 0 ? <p>Tidak ada aset</p> : (<>
-                    { sortData(dataAset).map((data, index)=>{
-                      return (
-                        <div className="card" key={index}>
-                          <div className="card__header">
-                            <div className="card__icon">
-                              <FaSeedling className='card__logo' />
-                            </div>
-                            <div style={{marginLeft: '15px'}}>
-                              <b>{data.Record.varietasBenih}</b>
-                              <p className="card__timestamp">{formatDate(data.Record.tanggalTanam)}</p>
-                            </div>                
-                          </div>
-                          <div className="card__body">
-                            <div className="quantity-value">
-                              <div className="quantity">
-                                <span>Kuantitas</span>
-                                <p>{numberFormat(data.Record.kuantitasBenih)} Kg </p>
-                              </div>
-                            </div>
-                          </div>
-                          <div className="card__bottom">
-                              <Button className="openModalBtn" buttonSize={'btn--small'} buttonColor={'primary'}
-                                onClick={(e) => {
-                                  setModalOpen(true)
-                                  handleEdit(e, data.id)
-                                }}
-                                value={index}
-                              > 
-                              TAMBAH KUANTITAS
-                              </Button>
-                            {modalOpen && 
-                              <Modal setOpenModal={setModalOpen} 
-                                modalTitle={'Tambah Kuantitas'}  
-                                modalBody={
-                                  <>
-                                    <form id='editKuantitas' onSubmit={handleSubmit}>
-                                      {errorKuantitas && <span className='err'>{errorKuantitas}</span>}
-                                      <Input className='number' label={'Tambah Kuantitas Benih'} type='number' name='kuantitasBenihKg' id='kuantitasBenihKg' 
-                                      placeholder='Tambah Kuantitas Benih' value={inputTrx.kuantitasBenihKg} onChange={handleChange} onBlur={validateInput} 
-                                      errorMsg={error.kuantitasBenihKg} required />
-                                      <p>Data tidak bisa diubah menjadi lebih kecil dari saat ini</p>
-                                      <div style={{marginTop : '10px'}}>
-                                        <label>
-                                          <input type="checkbox" defaultChecked={false} onChange={handleCheck} /> Saya yakin mengubah data
-                                        </label>
-                                      </div>
-                                    </form>
-                                  </>
-                                } 
-                                cancelBtn ={'BATAL'}
-                                processBtn={'SIMPAN'}
-                                form='editKuantitas'
-                                setConfirmed={setConfirmed}
-                                disabled={confirmed}
-                              />
-                              }
-                          </div>
-                        </div>
-                      )
-                    })}
-                    </>)}
+                    <p>Tidak ada aset</p>
                   </UnlockAccess>
                   {/* END ASET PENANGKAR */}                  
 
